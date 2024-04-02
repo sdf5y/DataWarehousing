6457517d949db3ba662053461a35a3f1dcc7cb1a
sdf5y <122656037+sdf5y@users.noreply.github.com>
2024-04-02 16:36:40 -0400

    Add files via upload
    isolated collaboration and single artists

--- Project.py
+++ Project.py
@@ -1,8 +1,7 @@
 #%% libraries
 import numpy as np
 import pandas as pd
-
-!pip install faker
+#!pip install faker
 from faker import Faker
 #!pip install random
 import random
@@ -24,11 +23,13 @@ data_df[data_df.isnull().any(axis=1)] #looks like we have 1 NA
 
 data_df.describe().round(0)
 data_df.shape
-
 data_df.dtypes
+
+plt.figure(figsize=(10, 8))
 sns.heatmap(data_df.select_dtypes(include=["int", "float"]).corr(), annot=True, cmap="coolwarm", fmt=".2f")
 plt.title('Correlation Analysis')
 plt.show()
+
 # %%
 data_df.columns
 # %%
@@ -38,13 +39,27 @@ data_df.head()
 # %%
 sorted_df = data_df.sort_values('popularity', ascending = False)
 sorted_df.head()
-# %%
+# %% Top Ten Artists without Collaborations
 
-df_artists=sorted_df.drop_duplicates(subset='artists',keep='first')
+colab_index = df_artists['artists'].str.find(';')
+df_colab_artists = [df_artists['artists'].loc[i] for i in colab_index.index]
+single_artists_df = df_artists[df_artists['artists'].str.contains(";") == False]
+
+top_10_artists = single_artists_df.nlargest(10, 'popularity')
+top_10_artists
+
+# %% Top ten Artists including collaborations
+
+df_artists= sorted_df.drop_duplicates(subset='artists',keep='first')
 df_artists.head()
-# %%
+
 top_10_artists = df_artists.nlargest(10, 'popularity')
 top_10_artists.head()
+
+# %% Top ten Artists including collaborations
+top_10_artists = df_artists.nlargest(10, 'popularity')
+top_10_artists.head()
+
 # %%
 import plotly.express as px
 import plotly.io as pio
@@ -59,11 +74,15 @@ fig1.update_layout(title_text='Top 10 Artists in terms of Popularity', yaxis_tit
                    xaxis_title='Popularity (from 0-100)')
 fig1.show()
 # %%
+
+#%% 
 colab_artist = []
+artist_map = []
 for i in range(len(data_df)):
      cell = data_df.iloc[i]['artists']
+     cell1 = data_df.iloc[i]['artists']
      if isinstance(cell, str) and ";" in cell:
+          cell1 = np.ma.masked_where(isinstance(cell, str) and ";" in cell, cell)
           cell = cell.split(";")
      colab_artist.append(cell)
-
-# %%
+     artist_map.append(cell1)
