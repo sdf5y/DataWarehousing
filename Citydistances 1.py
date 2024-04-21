f581c4cf9914b876bc0240331de58772c265bb83
ChayaChandana28 <157654087+ChayaChandana28@users.noreply.github.com>
2024-04-21 15:16:59 -0400

    Add files via upload

--- Citydistances 1.py
+++ Citydistances 1.py
@@ -3,6 +3,8 @@
 from pymongo import MongoClient
 from faker import Faker
 import random
+import pandas as pd
+import numpy as np
 
 # Functions to Generate data
 fake = Faker()
@@ -35,6 +37,46 @@ for doc in collection.find().limit(5):
 
 client.close()
 
+#%% Adding the relevant cities as per the instructions.
+data_atlanta = {
+    'FromCity': ['Benderchester', 'Atlanta', 'Atlanta', 'Joshuaport', 'Atlanta','Lake Jorgeberg', 'Atlanta', 'Atlanta', 'Port Heidi', 'Atlanta'],
+    'ToCity': ['Atlanta', 'Montogomery', 'Pricefort', 'Atlanta', 'Hinesberg','Atlanta','North Arianastad','Phillipsfurt','Atlanta','West Ericton'],
+    'Distance': [100, 150, 200, 250, 300,350,400,450,500,550]
+}
+
+data_frankfurt =  {
+    'FromCity': ['Port Mary', 'Frankfurt','East Tiffany','Frankfurt','Frankfurt','Bethanyview','Frankfurt','Frankfurt','South Amy',
+                              'Frankfurt'],
+    'ToCity': ['Frankfurt', 'Turnerfurt', 'Frankfurt', 'North Lisa', 'Adamsshire','Frankfurt','Robertfort','New Kimberg','Frankfurt','Maryberg'],
+    'Distance': [100, 150, 200, 250, 300,350,400,450,500,550]
+}
+
+data_amman =   {'FromCity': ['Amman', 'Amman', 'Amman', 'Amman', 'Amman','Amman', 'Amman', 'Amman', 'Amman', 'Amman'],
+    'ToCity': ['North Sara', 'Peterton', 'Kathyton', 'Coxborough', 'Donaldport','North Paulside','Garzashire','South Ashley','Melissaview','Lake Jacob'],
+    'Distance': [103, 156, 273, 255, 389,353,410,454,523,557]
+               }
+
+df_atlanta = pd.DataFrame(data_atlanta)
+df_frankfurt = pd.DataFrame(data_frankfurt)
+df_amman = pd.DataFrame(data_amman)
+
+print(df_amman,df_atlanta,df_frankfurt)
+
+#%%
+#Generating the unique cities and distances
+
+df = pd.DataFrame()
+df = pd.concat([df, df_atlanta, df_frankfurt, df_amman], ignore_index=True)
+print(df)
+
+#%% Marging and Saving to csv
+main_df = pd.DataFrame(dataset, columns=["FromCity", "ToCity", "Distance"])
+print(main_df)
+#%%
+dataset = pd.concat([main_df, df], ignore_index=True)
+print(dataset)
+
+#final_df.to_csv('city_distances.csv', index=False)
 
 # %%
 #Task
@@ -60,7 +102,7 @@ for road in atlanta_roads:
 
 client.close()
 
-#Time taken to execute the query: 0.17115116119384766 seconds
+#Time taken to execute the query: 1.7097489833831787 seconds
 
 # %%
 #2 Find Roads Longer than 150 km, with Details
@@ -110,7 +152,7 @@ for result in total_length:
 
 client.close()
 
-#Time taken to execute the query: 0.5260400772094727 seconds
+#Time taken to execute the query: 5.0743818283081055 seconds
 
 # %%
 #4 Determine Shortest and Longest Road from 'Amman'
@@ -140,8 +182,8 @@ for road in longest_road:
 
 client.close()
 
-#Time taken to execute the query for shortest road: 0.0001862049102783203 seconds
-#Time taken to execute the query for longest road: 0.0001862049102783203 seconds
+#Time taken to execute the query for shortest road: 0.007380008697509766 seconds
+#Time taken to execute the query for longest road: 0.007380008697509766 seconds
 
 # %%
 
@@ -163,7 +205,7 @@ start_time = time.time()
 graph.run(load_csv_query)
 print(f"CSV Load Time: {time.time() - start_time:.2f} seconds")
 
-#Time taken to load CSV: 1549.78 seconds
+#Time taken to load CSV: 1943.05 seconds
 
 #%%
 # 1 List Roads from/to 'Atlanta' with Distances and Destinations
@@ -177,9 +219,9 @@ result1 = graph.run(query1)
 print("Roads from/to Atlanta:")
 for record in result1:
     print(record)
-print(f"Query Time: {time.time() - start_time:.2f} seconds")
+print(f"Query Time: {time.time() - start_time} seconds")
 
-#Time taken to execute the query: 0.04 seconds
+#Time taken to execute the query: 0.038472890853881836 seconds
 
 #%%
 #2 Find Roads Longer than 150 km, with Details
@@ -194,9 +236,9 @@ result3 = graph.run(query2)
 print("\nRoads longer than 150 km:")
 for record in result3:
     print(record)
-print(f"Query Time: {time.time() - start_time:.2f} seconds")
+print(f"Query Time: {time.time() - start_time} seconds")
 
-#Time taken to execute the query: 1.54 seconds
+#Time taken to execute the query: 3.2383840084075928 seconds
 
 #%%
 #3 Total Road Length Connected to 'Frankfurt'
@@ -209,9 +251,9 @@ start_time = time.time()
 result2 = graph.run(query3)
 print("\nTotal road length connected to Frankfurt:")
 print(result2)
-print(f"Query Time: {time.time() - start_time:.2f} seconds")
+print(f"Query Time: {time.time() - start_time} seconds")
 
-#Time taken to execute the query: 0.05 seconds
+#Time taken to execute the query: 0.04621005058288574 seconds
 
 #%%
 #4 Determine Shortest Road from 'Amman'
@@ -227,9 +269,9 @@ start_time = time.time()
 result4_shortest = graph.run(query4_shortest)
 print("\nShortest road from Amman:")
 print(result4_shortest)
-print(f"Query Time: {time.time() - start_time:.2f} seconds")
+print(f"Query Time: {time.time() - start_time} seconds")
 
-#Time taken to execute the query: 0.12 seconds
+#Time taken to execute the query: 0.04681897163391113 seconds
 
 #%%
 # Determine Longest Road from 'Amman'
@@ -245,17 +287,17 @@ start_time = time.time()
 result4_longest = graph.run(query4_longest)
 print("\nLongest road from Amman:")
 print(result4_longest)
-print(f"Query Time: {time.time() - start_time:.2f} seconds")
+print(f"Query Time: {time.time() - start_time} seconds")
 
-#Time taken to execute the query: 0.20 seconds
+#Time taken to execute the query: 0.06295394897460938 seconds
 
 # %%
 # Bar graph
 import matplotlib.pyplot as plt
 
 queries = ['List Roads', 'Find Roads > 150km', 'Total Road Length', 'Shortest Road', 'Longest Road']
-mongo_times = [0.17, 4.05, 0.52, 0.01, 0.01]  
-neo4j_times = [0.04, 1.54, 0.05, 0.12, 0.20]  
+mongo_times = [1.70, 4.05, 5.07, 0.01, 0.01]  
+neo4j_times = [0.03, 3.23, 0.04, 0.04, 0.06]  
 
 x = range(len(queries)) 
 width = 0.35  
