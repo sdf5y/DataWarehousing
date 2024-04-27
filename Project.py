994e06084115660542bc3a9a300a56f711979ddb
ChayaChandana28 <157654087+ChayaChandana28@users.noreply.github.com>
2024-04-26 21:56:37 -0400

    Add files via upload

--- Project.py
+++ Project.py
@@ -19,33 +19,38 @@ data_df = pd.read_csv('spotifydata.csv', index_col=0)
 
 #%% Summary Statistics 
 
+# Calculating summary statistics for each numeric column
+summary_stats = data_df.describe(include='all')
+
+# Printing the summary statistics
+print(summary_stats)
+
+
+#%%
+#Correlation Analysis
+
 data_df[data_df.isnull().any(axis=1)] #looks like we have 1 NA 
-data_df = data_df.dropna(axis=0) #lets drop this one null
+#data_df = data_df.dropna(axis=0) #lets drop this one null
 
-print(data_df.describe().round(0))
-print(data_df.shape)
+data_df.describe().round(0)
+data_df.shape
 data_df.dtypes
-
-#%% Coorelation Plot
 
 plt.figure(figsize=(10, 8))
 sns.heatmap(data_df.select_dtypes(include=["int", "float"]).corr(), annot=True, cmap="coolwarm", fmt=".2f")
 plt.title('Correlation Analysis')
 plt.show()
 
-#%% Convert Seconds durations
-
+# %%
+data_df.columns
+# %%
 data_df['duration_ms'] = (data_df['duration_ms'] / 1000)
 data_df.rename({'duration_ms': 'duration_sec'}, axis=1, inplace=True)
 data_df.head()
-
-#%% Top 5 Popularity
-
+# %%
 sorted_df = data_df.sort_values('popularity', ascending = False)
 sorted_df.head()
-
-#%% Top Ten Artists without Collaborations
-
+# %% Top Ten Artists without Collaborations
 df_artists= sorted_df.drop_duplicates(subset='artists',keep='first')
 
 colab_index = df_artists['artists'].str.find(';')
@@ -55,7 +60,7 @@ single_artists_df = df_artists[df_artists['artists'].str.contains(";") == False]
 top_10_artists = single_artists_df.nlargest(10, 'popularity')
 top_10_artists
 
-#%% Top ten Artists including collaborations
+# %% Top ten Artists including collaborations
 
 top_10_artists = df_artists.nlargest(10, 'popularity')
 top_10_artists.head()
@@ -64,8 +69,7 @@ top_10_artists.head()
 top_10_artists = df_artists.nlargest(10, 'popularity')
 top_10_artists.head()
 
-#%% Plot of Top 10 popular artists
-
+#%%
 import plotly.express as px
 import plotly.io as pio
 
@@ -73,14 +77,23 @@ pio.renderers.default = 'browser'
 
 fig1 = px.bar(top_10_artists, x='popularity', y='artists', text='popularity',
               color='popularity',color_continuous_scale='Viridis')  
-
 fig1.update_traces(marker_line_color='rgb(8,48,107)', marker_line_width=2,
                     opacity=0.8,)
-
 fig1.update_layout(title_text='Top 10 Artists in terms of Popularity', yaxis_title='Artists Names',
                    xaxis_title='Popularity (from 0-100)')
-
 fig1.show()
+
+#%% 
+colab_artist = []
+artist_map = []
+for i in range(len(data_df)):
+     cell = data_df.iloc[i]['artists']
+     cell1 = data_df.iloc[i]['artists']
+     if isinstance(cell, str) and ";" in cell:
+          cell1 = np.ma.masked_where(isinstance(cell, str) and ";" in cell, cell)
+          cell = cell.split(";")
+     colab_artist.append(cell)
+     artist_map.append(cell1)
 
 #%% Unique artists and popularity
 
@@ -88,8 +101,7 @@ total_weight = sum(df_artists['popularity'])
 normalized_weights = [weight / total_weight for weight in df_artists['popularity']]
 artists_list = df_artists['artists'].tolist()
 
-#%% Create fake user artist histories
-
+#%% Create fake user histories
 from faker import Faker
 import random
 
@@ -107,35 +119,34 @@ for i in range(1, 30):
         artist_plays_list.append(artist_plays)
     data.append((username, email, country, artist_plays_list))
 
-#%% Selecting Numerical columns for further analysis
-
+# %%
+# Selecting Numerical columns for further analysis
 num_cols = data_df[data_df.columns[(data_df.dtypes == 'float64') | (data_df.dtypes == 'int64')]]
 num_cols.shape
-
+# %%
 num_cols.info()
-#%% Checking distribution of numerical columns
-
+# %%
+#Checking distribution of numerical columns
 sns.set_style('darkgrid')
 sns.set(rc={"axes.facecolor":"#F2EAC5","figure.facecolor":"#F2EAC5"})
 num_cols.hist(figsize=(20,15), bins=30, xlabelsize=8, ylabelsize=8)
 plt.tight_layout()
 plt.show()
-
-#%% Logistical Regression for Mode
-
+# %%
+#Logistical Regression for Mode
 #!pip install statsmodel.api
 import statsmodels.api as sm
 from statsmodels.formula.api import glm
 
-model = glm(formula='explicit ~ popularity + energy + speechiness', 
-            data = df_artists, family = sm.families.Binomial())
-
+model = glm(formula='mode ~ popularity + energy', 
+            data = num_cols, family = sm.families.Binomial())
 model_fit = model.fit()
-
 print(model_fit.summary())
 
-#%% Linear Regression to Predict Popularity by sonic attributes only
+# %%
+num_cols.head
 
+# %% Linear Regression to Predict Popularity by sonic attributes only
 import statsmodels.api as sm
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -155,8 +166,7 @@ X_test = X_test.astype(int)
 model = sm.OLS(y_train, X_train).fit()
 print(model.summary())
 
-#%% Linear Regression to Predict Popularity by Cultural attributes only
-
+# %% Linear Regression to Predict Popularity by Cultural attributes only
 import statsmodels.api as sm
 
 X = df_artists[['explicit', 'danceability', 'energy', 'key', 
@@ -176,3 +186,65 @@ X_test = X_test.astype(int)
 
 model = sm.OLS(y_train, X_train).fit()
 print(model.summary())
+# %%
+# Neo4j
+
+from neo4j import GraphDatabase
+
+class SpotifyLoader:
+    def __init__(self, uri, user, password):
+        print(f"Connecting to Neo4j at {uri} with username {user}")
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        print("Closing connection")
+        self.driver.close()
+
+    def load_spotify_data(self, file_url):
+        print(f"Loading data from {file_url}")
+        with self.driver.session() as session:
+            session.execute_write(self._load_data, file_url)
+            print("Data load complete")
+
+    @staticmethod
+    def _load_data(tx, file_url):
+        print("Executing Cypher query")
+        query = """
+        LOAD CSV WITH HEADERS FROM $file_url AS row
+        MERGE (n1:Artists {name: row.artists})
+        MERGE (n2:Album {name: row.album_name})
+        MERGE (n3:Track {
+            name: row.track_name, 
+            genre: row.track_genre, 
+            popularity: toInteger(row.popularity), 
+            duration: toInteger(row.duration_ms), 
+            danceability: toFloat(row.danceability), 
+            energy: toFloat(row.energy),
+            loudness: toFloat(row.loudness),
+            speechiness: toFloat(row.speechiness), 
+            acousticness: toFloat(row.acousticness), 
+            instrumentalness: toFloat(row.instrumentalness), 
+            liveness: toFloat(row.liveness), 
+            valence: toFloat(row.valence), 
+            tempo: toFloat(row.tempo)
+        })
+        MERGE (n1)-[:featured]->(n2)
+        MERGE (n3)-[:part_of]->(n2)
+        """
+        tx.run(query, file_url=file_url)
+
+if __name__ == "__main__":
+    uri = "neo4j://localhost:7687"
+    user = "neo4j"
+    password = "12345678"  # Replace with your actual password
+    file_url = "http://localhost:11001/project-cc095a88-cf6a-4151-b5fa-48307493a4ea/spotifydata.csv"
+
+    loader = SpotifyLoader(uri, user, password)
+    try:
+        loader.load_spotify_data(file_url)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        loader.close()
+
+#%%
